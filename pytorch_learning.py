058e6f309e9f091eb2722fc8aaf80b4e30cf8f69
Pavel Hajek <xhajekp00@vutbr.cz>
2025-11-25 23:32:49 +0100

    Added evaluation inside the test model

--- pytorch_learning.py
+++ pytorch_learning.py
@@ -1,47 +1,52 @@
 import torch
 from torch import nn
-import torchvision
+from torchvision import transforms, datasets
 import matplotlib.pyplot as plt
 
-batch_size = 4
+transform = transforms.ToTensor()
 
-transform = torchvision.transforms.ToTensor()
+train_set = datasets.MNIST(root='data', train=True, download=True, transform=transform)
+test_set = datasets.MNIST(root='data', train=False, download=True, transform=transform)
 
-trainset = torchvision.datasets.MNIST(root='data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
-
-# print(images.shape)
-
-image_size = 28 * 28
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=64, shuffle=False)
 
 class DigitRecognize(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(image_size, 64)
+        self.fc1 = nn.Linear(28 * 28, 64)
         self.fc2 = nn.Linear(64, 10)
         self.activation = nn.ReLU()
 
     def forward(self, x):
-        x = x.view(-1, image_size)
+        x = x.view(-1, 28 * 28) 
         return self.fc2(self.activation(self.fc1(x)))
 
 
 model = DigitRecognize()
 criterion = nn.CrossEntropyLoss()
-optimizer2 = torch.optim.SGD(model.parameters(), lr=0.001)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 epochs = 3
 
 for epoch in range(epochs):
-    correct = 0
-    for images, labels in trainloader:
+    print("starting epoch:", epoch)
+    for image, label in train_loader:
         optimizer.zero_grad()
-        pred = model(images)
-        loss = criterion(pred, labels)
+        pred = model(image)
+        loss = criterion(pred, label)
         loss.backward()
         optimizer.step()
-        correct += torch.sum(pred.argmax(dim=1) == labels).item()
+    print("epoch", epoch, "finished")
 
-    print(f"Epoch {epoch+1} complete with {correct} correct!")
+correct = 0
+total = 0
 
+print("starting evaluation")
+with torch.no_grad():
+    for image, label in test_loader:
+        preds = model(image).argmax(dim=1)
+        correct += (preds == label).sum().item()
+        total += label.size(0)
+
+print("\nfinal accuracy:", correct / total * 100, "%")
